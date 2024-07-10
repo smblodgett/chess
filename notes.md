486a21207924a36916f1b219123e9cbd3f968dde
Steven Blodgett <blodgett.steven.m@gmail.com>
2024-07-10 13:20:53 -0600

    refactored resetBoard()

--- notes.md
+++ notes.md
@@ -15,3 +15,14 @@ shallow copy: creates a new reference to the data object
 deep copy: creates a completely new data object. DO NOT confuse shallow and deep copying!
 syntax: public Object clone() {return new Object(this)}
 
+**7/10/24: PRINCIPLES OF DESIGN**
+1. Design is inherently iterative! 
+   1. You can never design 100% before you start coding. 
+   2. You should try and use design and building in a synergistic loop.
+2. Abstraction! 
+   1. Our job is to design a new language, in a way, that raises the level of abstraction to the thing you're working on. Rather than representing, say, DNA with string classes, you should create a DNA class that fits what DNA is better.
+   2. You should be able to do something without understanding how it works internally.
+   3. Since classes are usually complex, real-world things, you must be wise about what to include and what to leave out.
+3. Good naming!
+4. Single-responsibility principle!
+   1. each function/variable should only do one thing!
--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -75,104 +75,38 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        board = new ChessPiece[9][9]; // clear everything from board
-        // add the rows of pawns
-        //System.out.println("board has been reset!!");
-        for (int i=1;i<=8;i++){
-            // add white pawns
-            ChessPosition whitePawnPos = new ChessPosition(2,i);
-            ChessPiece whitePawn = new ChessPiece(ChessGame.TeamColor.WHITE,ChessPiece.PieceType.PAWN);
-            addPiece(whitePawnPos,whitePawn);
-
-            // add black pawns
-            ChessPosition blackPawnPos = new ChessPosition(7,i);
-            ChessPiece blackPawn = new ChessPiece(ChessGame.TeamColor.BLACK,ChessPiece.PieceType.PAWN);
-            addPiece(blackPawnPos,blackPawn);
-        }
-
-        // add knights
-        for (int i=1;i<=8;i++){
-            // knights only belong on the 2nd and 7th files
-            if (i==1||i==3||i==4||i==5||i==6||i==8) {
-                continue;
+        board = new ChessPiece[9][9];
+        for (int i=1;i<9;i++){
+            // add pawns
+            addPiece(new ChessPosition(2,i),new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN));
+            addPiece(new ChessPosition(7,i),new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN));
+            // add rooks
+            if (i==1||i==8){
+                addPiece(new ChessPosition(1,i),new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK));
+                addPiece(new ChessPosition(8,i),new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK));
             }
-            // add white knights
-            ChessPosition whiteKnightPos = new ChessPosition(1,i);
-            ChessPiece whiteKnight = new ChessPiece(ChessGame.TeamColor.WHITE,ChessPiece.PieceType.KNIGHT);
-            addPiece(whiteKnightPos,whiteKnight);
-
-            // add black knights
-            ChessPosition blackKnightPos = new ChessPosition(8,i);
-            ChessPiece blackKnight = new ChessPiece(ChessGame.TeamColor.BLACK,ChessPiece.PieceType.KNIGHT);
-            addPiece(blackKnightPos,blackKnight);
-        }
-
-        // add bishops
-        for (int i=1;i<=8;i++){
-            // bishops only belong on the 3rd and 6th files
-            if (i==1||i==2||i==4||i==5||i==7||i==8) {
-                continue;
+            // add knights
+            if (i==2||i==7){
+                addPiece(new ChessPosition(1,i),new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT));
+                addPiece(new ChessPosition(8,i),new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT));
             }
-            // add white bishops
-            ChessPosition whiteBishopPos = new ChessPosition(1,i);
-            ChessPiece whiteBishop = new ChessPiece(ChessGame.TeamColor.WHITE,ChessPiece.PieceType.BISHOP);
-            addPiece(whiteBishopPos,whiteBishop);
-
-            // add black bishops
-            ChessPosition blackBishopPos = new ChessPosition(8,i);
-            ChessPiece blackBishop = new ChessPiece(ChessGame.TeamColor.BLACK,ChessPiece.PieceType.BISHOP);
-            addPiece(blackBishopPos,blackBishop);
-        }
-
-        // add rooks
-        for (int i=1;i<=8;i++){
-            // rooks only belong on the first and eighth files
-            if (i==2||i==3||i==4||i==5||i==6||i==7) {
-                continue;
+            // add bishops
+            if (i==3||i==6){
+                addPiece(new ChessPosition(1,i),new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP));
+                addPiece(new ChessPosition(8,i),new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP));
             }
-            // add white rooks
-            ChessPosition whiteRookPos = new ChessPosition(1,i);
-            ChessPiece whiteRook = new ChessPiece(ChessGame.TeamColor.WHITE,ChessPiece.PieceType.ROOK);
-            addPiece(whiteRookPos,whiteRook);
-
-            // add black rooks
-            ChessPosition blackRookPos = new ChessPosition(8,i);
-            ChessPiece blackRook = new ChessPiece(ChessGame.TeamColor.BLACK,ChessPiece.PieceType.ROOK);
-            addPiece(blackRookPos,blackRook);
-        }
-
-        // add queens
-        for (int i=1;i<=8;i++){
-            // queens only belong on the 4th file
-            if (i==1||i==2||i==3||i==5||i==6||i==7||i==8) {
-                continue;
+            // add queens
+            if (i==4) {
+                addPiece(new ChessPosition(1, i), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.QUEEN));
+                addPiece(new ChessPosition(8, i), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN));
             }
-            // add white queen
-            ChessPosition whiteQueenPos = new ChessPosition(1,i);
-            ChessPiece whiteQueen = new ChessPiece(ChessGame.TeamColor.WHITE,ChessPiece.PieceType.QUEEN);
-            addPiece(whiteQueenPos,whiteQueen);
-
-            // add black queen
-            ChessPosition blackQueenPos = new ChessPosition(8,i);
-            ChessPiece blackQueen = new ChessPiece(ChessGame.TeamColor.BLACK,ChessPiece.PieceType.QUEEN);
-            addPiece(blackQueenPos,blackQueen);
-        }
-
-        // add kings
-        for (int i=1;i<=8;i++){
-            // kings only belong on the 5th file
-            if (i==1||i==2||i==3||i==4||i==6||i==7||i==8) {
-                continue;
+            // add kings
+            if (i==5){
+                addPiece(new ChessPosition(1,i),new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING));
+                addPiece(new ChessPosition(8,i),new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KING));
             }
-            // add white king
-            ChessPosition whiteKingPos = new ChessPosition(1,i);
-            ChessPiece whiteKing = new ChessPiece(ChessGame.TeamColor.WHITE,ChessPiece.PieceType.KING);
-            addPiece(whiteKingPos,whiteKing);
 
-            // add black king
-            ChessPosition blackKingPos = new ChessPosition(8,i);
-            ChessPiece blackKing = new ChessPiece(ChessGame.TeamColor.BLACK,ChessPiece.PieceType.KING);
-            addPiece(blackKingPos,blackKing);
+
         }
     }
 
