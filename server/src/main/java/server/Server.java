d898ef4fe36b908cc7813ca9136d098f09560fe8
Steven Blodgett <blodgett.steven.m@gmail.com>
2024-07-16 12:07:11 -0600

    made correct directories and files for phase 3

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,8 +1,10 @@
+package server;
+
 import chess.*;
 
-public class ServerMain {
+public class Server {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+        System.out.println("♕ 240 Chess server.Server: " + piece);
     }
 }
