ecf58120cbeff8b82a44e89bc55b98bb0c3ad446
Steven Blodgett <blodgett.steven.m@gmail.com>
2024-07-17 15:54:26 -0600

    Added handler, request, and response classes/packages

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,10 +1,25 @@
 package server;
 
-import chess.*;
+import spark.*;
 
 public class Server {
-    public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess server.Server: " + piece);
+
+    public int run(int desiredPort) {
+        Spark.port(desiredPort);
+
+        Spark.staticFiles.location("web");
+
+        // Register your endpoints and handle exceptions here.
+
+        //This line initializes the server and can be removed once you have a functioning endpoint
+        Spark.init();
+
+        Spark.awaitInitialization();
+        return Spark.port();
+    }
+
+    public void stop() {
+        Spark.stop();
+        Spark.awaitStop();
     }
 }
